686c1a4f655429d15f26775eea90e18468be0715
Andrew Richter <58954012+AndrewDRichter@users.noreply.github.com>
2025-11-17 12:05:22 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+function Home(){
+    return <main>
+        <h1>
+            Minha amada, Priscila.
+        </h1>
+        <p>
+            Priscila, minha amada <br />
+            Minha apreciada namorada <br />
+            Quero de ti cuidar, e, com muito esmero, <br />
+            Amar-te. E a única coisa que de ti espero, <br />
+            É que me deixes, ao altar, levar-te.
+        </p>
+        </main>
+}
+
+export default Home;
