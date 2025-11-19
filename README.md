1987ec36093aa2adb8d6ed41cf0a45f0f9fbde59
Andrew Richter <58954012+AndrewDRichter@users.noreply.github.com>
2025-11-19 17:23:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Criando um clone do https://www.tabnews.com.br/ seguindo a aula de github do https://curso.dev/
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
-function Home(){
-    return <main>
-        <h1>
-            Minha amada, Priscila.
-        </h1>
-        <p>
-            Priscila, minha amada,<br />
-            minha apreciada namorada,<br />
-            Quero de ti cuidar e, com muito esmero,<br />
-            amar-te. E a única coisa que de ti espero<br />
-            é que me deixes, ao altar, levar-te. ❤
-        </p>
-        </main>
+function Home() {
+  return (
+    <main>
+      <h1>Minha amada, Priscila.</h1>
+      <p>
+        Priscila, minha amada,
+        <br />
+        minha apreciada namorada,
+        <br />
+        Quero de ti cuidar e, com muito esmero,
+        <br />
+        amar-te. E a única coisa que de ti espero
+        <br />é que me deixes, ao altar, levar-te. ❤
+      </p>
+    </main>
+  );
+}
+
+function uno2tres() {
+  console.log("123");
 }
 
 export default Home;
