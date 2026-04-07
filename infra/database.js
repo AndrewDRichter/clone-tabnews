dd91362ee860849fe758f1f29030e63ed4258573
Andrew Richter <58954012+AndrewDRichter@users.noreply.github.com>
2026-04-07 20:41:21 +0000

    add services scripts

--- infra/database.js
+++ infra/database.js
@@ -2,10 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    user: "postgres",
-    host: "localhost",
-    port: 5432,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
   });
 
   await client.connect();
