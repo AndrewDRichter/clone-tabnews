d485f896f40fac0460f8537e42bef5f85c0df102
Andrew Richter <58954012+AndrewDRichter@users.noreply.github.com>
2026-04-10 00:55:06 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -11,12 +11,39 @@ async function query(queryObject) {
 
   await client.connect();
 
-  const result = await client.query(queryObject);
-  await client.end();
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return error;
+  } finally {
+    await client.end();
+  }
+}
 
-  return result;
+async function getDatabaseStatus() {
+  const dbVersionQuery = await query("SHOW server_version;");
+  const dbVersion = dbVersionQuery.rows[0].server_version;
+
+  const dbMaxConnectionsQuery = await query("SHOW max_connections;");
+  const dbMaxConn = parseInt(dbMaxConnectionsQuery.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbUsedConnectionsQuery = await query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dbUsedConn = dbUsedConnectionsQuery.rows[0].count;
+
+  return {
+    dbVersion: dbVersion,
+    dbMaxConn: dbMaxConn,
+    dbUsedConn: dbUsedConn,
+  };
 }
 
 export default {
   query: query,
+  getDatabaseStatus: getDatabaseStatus,
 };
