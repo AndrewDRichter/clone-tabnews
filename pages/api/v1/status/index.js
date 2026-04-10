ff434af5e704e8ccec9b717c730526b7e545b655
Andrew Richter <58954012+AndrewDRichter@users.noreply.github.com>
2026-04-10 00:56:00 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,20 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("Select 1 + 1 as somatória;");
-  console.log(result.rows);
-  response.status(200).json({ message: "Testando endpoint /status" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseStatus = await database.getDatabaseStatus();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseStatus.dbVersion,
+        max_connections: databaseStatus.dbMaxConn,
+        used_connections: databaseStatus.dbUsedConn,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const databaseVersion = responseBody.dependencies.database.version;
+  expect(databaseVersion).toEqual("16.13");
+
+  const dbMaxConnections = responseBody.dependencies.database.max_connections;
+  expect(dbMaxConnections).toEqual(100);
+
+  const dbUsedConnections = responseBody.dependencies.database.used_connections;
+  expect(dbUsedConnections).toEqual(1);
 });
