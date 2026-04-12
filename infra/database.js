462202a6513f171321034a1d20074cc6de1ec4d0
Andrew Richter <58954012+AndrewDRichter@users.noreply.github.com>
2026-04-12 19:19:38 +0000

    makes s  more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,15 +8,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
-
-  await client.connect();
+  console.log("Credenciais do Postgres: ", {
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
-    return error;
+    throw error;
   } finally {
     await client.end();
   }
