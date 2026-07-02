26d25cc647bfa6a1f2ac320b4449966d63c5efe4
Andrew Richter <58954012+AndrewDRichter@users.noreply.github.com>
2026-07-02 14:32:00 +0000

    trying to replicate the bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -30,7 +30,6 @@ export default async function migrations(request, response) {
     }
     return response.status(200).json(migratedMigrations);
   }
-  return response.status(200).json({ ok: false });
 
-  return response.status(405).end();
+  // return response.status(405).end();
 }
